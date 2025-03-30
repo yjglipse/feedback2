b56189cf991c6e4ff0367e6bab7b1627b50b85f7
moniquekathelin09 <moniquekathelin09@gmail.com>
2025-03-30 17:59:55 +0000

    finalização do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -45,11 +45,14 @@ export function App() {
            <button className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1" onClick={handleSubmit}>Submit</button>
          </div>
        ) : (
-        <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass">
-          <img src={illustrationThankYou} alt="illustration thank you" />
-
-          <p></p>
-        </div>
+          <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass text-center">
+           <img className="mx-auto mb-6" src={illustrationThankYou} alt="illustration thank you" />
+ 
+           <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl">You selected {rateNote} out of 5!</p>
+           <h1 className="font-bold text-2xl mb-2.5">Thank You!</h1>
+           <p className="text-light-gray text-sm leading-1">We appreciate you taking the time to give a rating. If you ever need more support, 
+             don’t hesitate to get in touch!</p>
+          </div>
        )
   )
 }
