8be77b32dd66b590331c984121de3ad4f3c728a8
yjglipse <143204907+yjglipse@users.noreply.github.com>
2025-05-05 14:21:27 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,9 @@ export function App() {
   const[submited, setSubmited] = useState(false) // = const submited = false
    const[rateNote, setRateNote] = useState(0)
  
-   function handleSubmit() {
-     if(rateNote !== 0) {
-       setSubmited(!submited)
-       return
-     }
- 
-     alert("Please, choose a rate button!")
-   }
- 
-   function handleChangeRateNote(value) {
-     setRateNote(value)
-   }
   return(
       submited ===  false ? (
-        <CardInicial handleChangeRateNote={handleChangeRateNote} handleSubmit={handleSubmit}/>
+        <CardInicial setRateNote={setRateNote} setSubmited={setSubmited} rateNote ={rateNote}/>
        ) : (
         <CardFinal rateNote={rateNote}/>
        )
